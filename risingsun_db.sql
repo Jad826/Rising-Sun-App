29347e5ec0bd6c8b7b0c796f95b55a1145e37278
Jad826 <jadmakke7@gmail.com>
2025-04-26 11:30:47 +0300

    remove comments in risingsun_db.sql

--- risingsun_db.sql
+++ risingsun_db.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 23, 2025 at 02:21 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.1.25
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -17,16 +8,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `risingsun_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `account`
---
-
 CREATE TABLE `account` (
   `id` int(3) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -36,22 +17,10 @@ CREATE TABLE `account` (
   `region` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `attribute`
---
-
 CREATE TABLE `attribute` (
   `id` int(3) NOT NULL,
   `atturibute_na` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `city`
---
 
 CREATE TABLE `city` (
   `id` int(3) NOT NULL,
@@ -62,22 +31,10 @@ CREATE TABLE `city` (
   `country_id` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `country`
---
-
 CREATE TABLE `country` (
   `id` int(3) NOT NULL,
   `country_na` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `measuring_units`
---
 
 CREATE TABLE `measuring_units` (
   `id` int(3) NOT NULL,
@@ -86,23 +43,11 @@ CREATE TABLE `measuring_units` (
   `attribute_id` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users_city`
---
-
 CREATE TABLE `users_city` (
   `users_id` int(3) NOT NULL,
   `city_id` int(3) NOT NULL,
   `added_on` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users_prefrences`
---
 
 CREATE TABLE `users_prefrences` (
   `users_id` int(3) NOT NULL,
@@ -110,102 +55,48 @@ CREATE TABLE `users_prefrences` (
   `atturbute_id` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `weather_status`
---
-
 CREATE TABLE `weather_status` (
   `id` int(3) NOT NULL,
   `weather_st` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `account`
---
 ALTER TABLE `account`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `attribute`
---
 ALTER TABLE `attribute`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `city`
---
 ALTER TABLE `city`
   ADD PRIMARY KEY (`id`),
   ADD KEY `country_id` (`country_id`);
 
---
--- Indexes for table `country`
---
 ALTER TABLE `country`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `measuring_units`
---
 ALTER TABLE `measuring_units`
   ADD PRIMARY KEY (`id`),
   ADD KEY `attribute_id` (`attribute_id`);
 
---
--- Indexes for table `users_city`
---
 ALTER TABLE `users_city`
   ADD PRIMARY KEY (`users_id`,`city_id`),
   ADD KEY `CITYID` (`city_id`);
 
---
--- Indexes for table `users_prefrences`
---
 ALTER TABLE `users_prefrences`
   ADD PRIMARY KEY (`users_id`,`atturbute_id`),
   ADD KEY `meassuring_units_i` (`meassuring_units_i`);
 
---
--- Indexes for table `weather_status`
---
 ALTER TABLE `weather_status`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `account`
---
 ALTER TABLE `account`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `city`
---
 ALTER TABLE `city`
   ADD CONSTRAINT `COUNTRY` FOREIGN KEY (`country_id`) REFERENCES `account` (`id`) ON DELETE CASCADE;
 
---
--- Constraints for table `measuring_units`
---
 ALTER TABLE `measuring_units`
   ADD CONSTRAINT `attiid` FOREIGN KEY (`attribute_id`) REFERENCES `attribute` (`id`) ON DELETE CASCADE;
 
---
--- Constraints for table `users_city`
---
 ALTER TABLE `users_city`
   ADD CONSTRAINT `CITYID` FOREIGN KEY (`city_id`) REFERENCES `city` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `users_id` FOREIGN KEY (`users_id`) REFERENCES `account` (`id`);
