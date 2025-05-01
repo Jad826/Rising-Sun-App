e5cdc44c9aa26a9bd250cad2d832919dc350b416
Jad826 <jadmakke7@gmail.com>
2025-05-01 18:58:30 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mysql.connector
 
 def insert_data(name, email, password):
@@ -19,26 +18,4 @@ def insert_data(name, email, password):
         if db.is_connected():
             cursor.close()
             db.close()
-=======
-import mysql.connector
-
-def insert_data(name, email, password):
-    try:
-        db = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="risingsun_db"
-        )
-
-        cursor = db.cursor()
-
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        
-    finally:
-        if db.is_connected():
-            cursor.close()
-            db.close()
->>>>>>> e2fe9677d9ba4041e20f3b3b2fe8dc23df12bfe1
             print("MySQL connection is closed")
