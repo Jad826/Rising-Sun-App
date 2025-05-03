4c9a2ab748366c9a6af8a8b0c14ad52f41b5238d
ghadimezher78 <122824072+ghadimezher78@users.noreply.github.com>
2025-05-03 06:46:24 -0700

    fixed errors

--- Register_Login.py
+++ Register_Login.py
@@ -15,8 +15,8 @@ cursor = db.cursor()
 def register():
     name = input("put your name: ")
     password = input("put your password: ")
-    contactnum = int(input("put you contactnumber: ")
-    cursor.execute("INSERT INTO account (username, password, contact_num) VALUES (%s, %s, %s)"(name, password, contact_num))
+    contactnum = int(input("put you contactnumber: "))
+    cursor.execute("INSERT INTO account (username, password, contact_num) VALUES (%s, %s, %s)"(name, password, contactnum))
     db.commit()
     locate_client.geolocate()                 
                    
