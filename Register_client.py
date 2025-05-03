ff163ff95e6c2e5c632c71d4febc49bda04a4493
ghadimezher78 <122824072+ghadimezher78@users.noreply.github.com>
2025-05-03 05:21:33 -0700

    Create Register_client.py

---
+++ Register_client.py
@@ -0,0 +1,8 @@
+import Register_Login
+menu = int(input("Welcome to the python client test api weather! choose your option \n 1.Register \n 2.Login \n 3.Exit \n what do you want?: ")
+if menu == 1:
+   Register_Login.Register()  
+elif menu == 2:
+   Register_Login.Login()
+elif menu == 3:
+   exit()  
