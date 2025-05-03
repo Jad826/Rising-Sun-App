0a717d3889f29a2c09aea80e4e214695218f258b
ghadimezher78 <122824072+ghadimezher78@users.noreply.github.com>
2025-05-03 06:50:12 -0700

    Update Register_client.py
    changed to correct functions

--- Register_client.py
+++ Register_client.py
@@ -1,8 +1,8 @@
 import Register_Login
-menu = int(input("Welcome to the python client test api weather! choose your option \n 1.Register \n 2.Login \n 3.Exit \n what do you want?: ")
+menu = int(input("Welcome to the python client test api weather! choose your option \n 1.Register \n 2.Login \n 3.Exit \n what do you want?: "))
 if menu == 1:
-   Register_Login.Register()  
+   Register_Login.register()  
 elif menu == 2:
-   Register_Login.Login()
+   Register_Login.login()
 elif menu == 3:
    exit()  
