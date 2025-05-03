80ae143690528548347eab8d8dc79348fea4a1fa
Jad826 <jadmakke7@gmail.com>
2025-05-03 13:53:08 +0300

    Cleaned locate_client.py Code

--- locate_client.py
+++ locate_client.py
@@ -1,15 +1,17 @@
 import requests
 from geopy.geocoders import Nominatim
 
+#Request user location 
 ip_data = requests.get("https://ipinfo.io/json").json()
 location = ip_data['loc']
 latitude, longitude = map(float, location.split(','))
 
+#Get address based on lat(latitude) and lon(longitude)
 geolocator = Nominatim(user_agent="geoapi")
 address = geolocator.reverse((latitude, longitude))
 
-
-input("Welcome To The Testing Python Client For The Geolocation API\nPress Enter to continue...")
+#User Interface
+input("\n\nWelcome To The Testing Python Client For The Geolocation API\nPress Enter to continue...")
 print("Your IP address is: ", ip_data['ip'])
 print("Your location is: ", ip_data['loc'])
 print(address)
