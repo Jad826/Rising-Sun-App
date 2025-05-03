ada3ba19826d4c5c8e8de6f363a48d6a6751be8b
ghadimezher78 <122824072+ghadimezher78@users.noreply.github.com>
2025-05-03 05:27:00 -0700

    Update Register_Login.py

--- Register_Login.py
+++ Register_Login.py
@@ -13,17 +13,20 @@ cursor = db.cursor()
 
 
 def register():
-  name = input("put your name: ")
-  password = input("put your password: ")
-  contactnum = int(input("put you contactnumber: ")
-  cursor.execute("INSERT INTO account (username, password, contact_num) VALUES (%s, %s, %s)"(name, password, contact_num))
-  db.commit()
+    name = input("put your name: ")
+    password = input("put your password: ")
+    contactnum = int(input("put you contactnumber: ")
+    cursor.execute("INSERT INTO account (username, password, contact_num) VALUES (%s, %s, %s)"(name, password, contact_num))
+    db.commit()
+    locate_client.geolocate()                 
                    
 def login():
     name = input("put your name: ")
     password = input("put your password: ")
     cursor.execute("SELECT * FROM account WHERE username = %s AND password = %s", (name, password))
     db.commit()
+    locate_client.geolocate()          
+              
               
 
 
