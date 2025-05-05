229ed5337264ff28dce61617f5b7f49178ea9ac8
ghadimezher78 <122824072+ghadimezher78@users.noreply.github.com>
2025-05-05 06:45:31 -0700

    Create menu_handler.py

---
+++ menu_handler.py
@@ -0,0 +1,11 @@
+
+import Register_Login
+
+def menu():
+    menu = int(input("Welcome to the python client test API weather! Choose your option \n 1.Register \n 2.Login \n 3.Exit \n What do you want?: "))
+    if menu == 1:
+        Register_Login.register()
+    elif menu == 2:
+        Register_Login.login()
+    elif menu == 3:
+        exit()
