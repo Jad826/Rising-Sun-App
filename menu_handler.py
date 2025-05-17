addb4ba98197ead8d067ed3f7cf962af1471d42f
Jad826 <jadmakke7@gmail.com>
2025-05-17 11:03:49 +0300

    Update menu_handler.py

--- menu_handler.py
+++ menu_handler.py
@@ -2,7 +2,7 @@
 import Register_Login
 
 def menu():
-    menu = int(input("Welcome to the python client test API weather! Choose your option \n 1.Register \n 2.Login \n 3.Exit \n What do you want?: "))
+    menu = int(input("Welcome to the python client test Login Register! Choose your option \n 1.Register \n 2.Login \n 3.Exit \n What do you want?: "))
     if menu == 1:
         Register_Login.register()
     elif menu == 2:
