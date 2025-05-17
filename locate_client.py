a5621bea7adf42cb5a45b64a0fb93ddf52bdcba1
ghadimezher78 <122824072+ghadimezher78@users.noreply.github.com>
2025-05-17 03:12:09 -0700

    Update locate_client.py

--- locate_client.py
+++ locate_client.py
@@ -1,6 +1,17 @@
 #Imports
+import mysql.connector
+import time
 import requests
 from geopy.geocoders import Nominatim
+
+db = mysql.connector.connect(
+  host = "localhost",
+    user="root",
+    password="",
+    database="risingsun_db"
+)
+
+cursor = db.cursor()
 
 #Request user location 
 ip_data = requests.get("https://ipinfo.io/json").json()
@@ -11,11 +22,26 @@ latitude, longitude = map(float, location.split(','))
 geolocator = Nominatim(user_agent="geoapi")
 address = geolocator.reverse((latitude, longitude))
 
-
-
-#User Interface
-def geolocator():
-  input("\n\nWelcome To The Testing Python Client For The Geolocation API\nPress Enter to continue...")
+def SeeLocation():
   print("Your IP address is: ", ip_data['ip'])
   print("Your location is: ", ip_data['loc'])
   print(address)
+  time.sleep(2)
+  geolocator()
+
+def OtherUsersloc():
+  menu = input("do you want to see others pepole location: ")
+  if menu == "yes":
+    cursor.execute("SELECT username, region FROM `account`")  
+    accounts = cursor.fetchall()
+    for username, region in accounts:
+      print(f"Username: {username}, Region: {region}")
+      
+#User Interface
+def geolocator():
+  menu = int(input("\n\nWelcome To The Testing Python Client For RisingSun! Choose The 3 Options\n 1.See Your Location \n 2.See Other Users \n 3.See Weather In Other Location \n 4.Exit \n What Do You Choose: "))
+  if menu == 1:
+    SeeLocation()
+  elif menu == 2:
+    OtherUsersloc()  
+ 
