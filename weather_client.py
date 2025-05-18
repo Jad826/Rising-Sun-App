9d8326a6513a482fce19377aba2b4ee855140dfc
Jad826 <jadmakke7@gmail.com>
2025-05-18 16:17:14 +0300

    Update weather_client.py

--- weather_client.py
+++ weather_client.py
@@ -7,18 +7,35 @@ location = ip_data['loc']
 latitude, longitude = map(float, location.split(','))
 
 # API key
-api_key = "your_api_key_here"  # Replace with your OpenWeatherMap API key(get on https://openweathermap.org/api) just make an account and verify through email and get the key
+api_key = "your api key"  # Replace with your OpenWeatherMap API key(get on https://openweathermap.org/api) just make an account and verify through email and get the key
 
 #Cities
 city1 = "Saida"
 city2 = "Beirut"
 city3 = "Nabatieh"
 
-#Base URL
-url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric"
+#Base URLs
+user_url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric"
+saida_url = f"https://api.openweathermap.org/data/2.5/weather?q={city1}&appid={api_key}&units=metric"
+beirut_url = f"https://api.openweathermap.org/data/2.5/weather?q={city2}&appid={api_key}&units=metric"
+nabatieh_url = f"https://api.openweathermap.org/data/2.5/weather?q={city3}&appid={api_key}&units=metric"
 
-#Request
-response = requests.get(url)
+#User Interface
+ui=int(input("\n\nWelcome To The Testing Python Client For The Geolocation API\n · Enter 1 to see your region's weather\n · Enter 2 to see the weather of Saida\n · Enter 3 to see the weather of Beirut\n · Enter 4 to see the weather of Nabatieh\n · Enter 5 to exit\n"))
+if ui == 1:
+    response = user_url
+elif ui == 2:
+    response = saida_url
+elif ui == 3:
+    response = beirut_url
+elif ui == 4:
+    response = nabatieh_url
+elif ui == 5:
+    print("Exiting...")
+    exit()
+else:
+    print("Invalid input. Please enter a number between 1 and 5.")
+    ui=int(input("\n\nWelcome To The Testing Python Client For The Geolocation API\n · Enter 1 to see your region's weather\n · Enter 2 to see the weather of Saida\n · Enter 3 to see the weather of Beirut\n · Enter 4 to see the weather of Nabatieh\n · Enter 5 to exit\n"))
 
 #Show the error if not working and if working show the data
 if response.status_code == 200:
